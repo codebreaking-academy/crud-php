16eb0a8b4f63df8721148335f444be6f774dfaf7
Sambeau <sambeauprak@gmail.com>
2023-04-23 12:57:45 +0200

    API

--- api/categories.php
+++ api/categories.php
@@ -3,6 +3,6 @@ header("Content-type: application/json; charset=utf-8");
 include '../functions.php';
 $query = $pdo->prepare('SELECT * FROM categorie');
 $query->execute();
-$produits = $query->fetchAll(PDO::FETCH_ASSOC);
+$categories = $query->fetchAll(PDO::FETCH_ASSOC);
 
-echo json_encode($produits);
+echo json_encode($categories);
