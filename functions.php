d36fa27420fa516797ce71fff43eb451b1028be8
Sambeau <sambeauprak@gmail.com>
2023-04-23 12:56:42 +0200

    Change BDD config

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function pdo_connect_mysql()
 	$DATABASE_HOST = 'localhost';
 	$DATABASE_USER = 'root';
 	$DATABASE_PASS = '';
-	$DATABASE_NAME = 'phpproduit';
+	$DATABASE_NAME = 'instant_gaming';
 	try {
 		return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
 	} catch (PDOException $exception) {
