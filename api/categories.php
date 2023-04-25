83c4b09db7c0c3e46d75591695c9911181baaa8a
Sambeau <sambeauprak@gmail.com>
2023-04-25 17:59:53 +0200

    Add new routes

--- api/categories.php
+++ api/categories.php
@@ -1,6 +1,15 @@
 <?php
 header("Content-type: application/json; charset=utf-8");
 include '../functions.php';
+
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $query = $pdo->prepare('SELECT a.* , c.type FROM categorie c  join produits a on c.id = a.categorie_id WHERE c.id = ? ');
+    $query->execute([$_GET['id']]);
+    $produit = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($produit);
+    exit();
+}
+
 $query = $pdo->prepare('SELECT * FROM categorie');
 $query->execute();
 $categories = $query->fetchAll(PDO::FETCH_ASSOC);
--- api/tags.php
+++ api/tags.php
@@ -1,6 +1,15 @@
 <?php
 header("Content-type: application/json; charset=utf-8");
 include '../functions.php';
+
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $query = $pdo->prepare('SELECT a.* , c.tag FROM tag c  join produits a on c.id = a.tag_id WHERE c.id = ? ');
+    $query->execute([$_GET['id']]);
+    $produit = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($produit);
+    exit();
+}
+
 $query = $pdo->prepare('SELECT * FROM tag');
 $query->execute();
 $tags = $query->fetchAll(PDO::FETCH_ASSOC);
